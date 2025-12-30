0d322851c32f5707ad2642f62ee3e7596e96b457
aheschl1 <ajheschl@gmail.com>
2025-12-29 23:08:56 -0500

    Change default path to "."

--- src/cli.rs
+++ src/cli.rs
@@ -8,38 +8,38 @@ pub(crate) enum Args {
     /// Initializes a new repository. 
     Init {
         
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String
     },
     /// Seals a repository, encrypting it.
     Seal {
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String
     },
     /// Unseals a repository, decrypting it.
     Unseal {
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String
     },
     /// Syncs local changes with the cloud.
     Sync {
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String
     },
     /// Links the repository with a remote branch.
     Link {
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String,
         url: String
     },
     /// Pulls a remote vault down to local.
     Pull {
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String,
         /// The git url (SSH-form) to pull from.
@@ -49,7 +49,7 @@ pub(crate) enum Args {
     /// will automatically seal again. This is highly recommended instead
     /// of manually sealing and unsealing.
     Open {
-        #[arg(short, long, default_value="")]
+        #[arg(short, long, default_value=".")]
         /// The target directory.
         target: String,
     }
